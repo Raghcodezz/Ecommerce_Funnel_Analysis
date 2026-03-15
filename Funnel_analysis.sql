c0203c55839391f3abc8b0ddd3f90b556ba549b4
P. Shiva Raghavendra Kumar <raghav113222@gmail.com>
2026-03-16 04:13:19 +0530

    Add funnel analysis SQL query for user conversion rates

---
+++ Funnel_analysis.sql
@@ -0,0 +1,16 @@
+with cte as(select 
+pagetype,
+count(userid) as user_count
+from ecommerce_journey
+group by pagetype
+)
+select 
+pagetype,
+user_count,
+cast(round(user_count*100.0/(select 
+count(userid) as user_count
+from ecommerce_journey
+where pagetype='home'),2) as decimal(10,2))
+as overall_conversion
+from cte
+order by user_count desc
